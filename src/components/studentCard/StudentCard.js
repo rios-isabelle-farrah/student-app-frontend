04118ef7459851bc2141359f251d298885e8c73c
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2023-10-03 13:22:38 -0400

    start styling StudentList and StudentCard

---
+++ src/components/studentCard/StudentCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./StudentCard.scss";
+
+const StudentCard = ({ student }) => {
+  //hooks or data we are grabbing
+  const { pic, firstName, lastName, company, skill, email, grades } = student;
+
+  // functions
+
+  const calculateAverage = (grades) => {
+    // one way to calculate average
+    // let sum = 0
+    // grades.map(grade => {
+    //     sum += Number(grade);
+    // })
+    // return sum /grades.length
+    // second option to calculate average
+    // return grades.reduce((sum,val) => sum + Number(val), 0)/grades
+    // the 0 in reduce function tells function that number needs to be zero at start of function
+    // third option to calculate average..cleaner option
+    const sum = grades.reduce((sum, val) => sum + Number(val), 0);
+    return sum / grades.length;
+  };
+
+  return (
+    <div className="studentCard">
+      <div>
+        <img src={pic} alt="headshot" />
+      </div>
+      <div className="studentCard__name">{`${firstName} ${lastName}`}</div>
+      <div className="studentCard__email">Email:{email}</div>
+      <div className="studentCard_company">Company:{company}</div>
+      <div className="skill">Skill:{skill}</div>
+      <div className="average">Average: {calculateAverage(grades).toFixed(2)}%</div>
+    </div>
+  );
+};
+
+export default StudentCard;
---
+++ src/components/studentList/StudentList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import StudentCard from "../studentCard/StudentCard";
+import './StudentList.scss'
+
+const StudentList = () => {
+  //hooks
+
+  const [students, setStudents] = useState([]);
+
+  //functions
+  useEffect(() => {
+    const url = `https://student-app-backend-october-422a553e0d4f.herokuapp.com/students`;
+
+    // reach out to backend and get our students and update our students hook with the new data
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setStudents(data.students);
+      });
+  }, []); //empty array means run on mount
+
+  //return or jsx 
+
+  return (
+    <div>
+      {students.map((student) => {
+    //  console.log(i)
+        return (
+          <div className="studentList">
+            <StudentCard student={student}/>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StudentList;
